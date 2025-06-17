cf7e1081f617ed488b3fc0f742dd5f1f56e1aa62
terafin <jwood@me.com>
2025-06-17 09:24:13 -0700

    Update sensor.py

--- custom_components/homeassistant_energy_collector/sensor.py
+++ custom_components/homeassistant_energy_collector/sensor.py
@@ -1,5 +1,9 @@
 from datetime import datetime
-from homeassistant.components.sensor import SensorEntity, SensorDeviceClass, SensorStateClass
+from homeassistant.components.sensor import (
+    SensorEntity,
+    SensorDeviceClass,
+    SensorStateClass,
+)
 from homeassistant.helpers.event import async_track_state_change_event, async_track_time_change
 import logging
 
@@ -7,8 +11,8 @@ _LOGGER = logging.getLogger(__name__)
 DOMAIN = "homeassistant_energy_collector"
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    source = entry.data["entity_id"]
     name = entry.data["name"]
+    source = entry.data["entity_id"]
     entity = WorkingEnergySensor(hass, name, source)
     entity._async_set_entry_id(entry.entry_id)
     async_add_entities([entity], update_before_add=True)
@@ -22,11 +26,20 @@ class WorkingEnergySensor(SensorEntity):
         self._last_update = None
         self._last_power = None
         self._attr_name = name
-        self._attr_unique_id = f"{source_entity_id.replace('.', '_')}_daily_kwh"
+        self._attr_unique_id = f"{DOMAIN}_{source_entity_id.replace('.', '_')}_daily_kwh"
         self._attr_native_unit_of_measurement = "kWh"
         self._attr_state_class = SensorStateClass.TOTAL_INCREASING
         self._attr_device_class = SensorDeviceClass.ENERGY
         self._attr_should_poll = False
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self._source_entity_id)},
+            "name": self._name,
+            "manufacturer": "Energy Collector",
+            "model": "Daily Accumulator",
+        }
 
     async def async_added_to_hass(self):
         self.async_on_remove(
@@ -39,23 +52,12 @@ class WorkingEnergySensor(SensorEntity):
                 self._hass, self._reset_daily, hour=0, minute=0, second=0
             )
         )
-        _LOGGER.debug(f"[{self._name}] Energy sensor added")
-
-    @property
-    def device_info(self):
-        return {
-            "identifiers": {("homeassistant_energy_collector", self._source_entity_id)},
-            "name": self._name,
-            "manufacturer": "Energy Collector",
-            "model": "Daily Accumulator",
-        }
 
     async def _handle_power_change(self, event):
         now = datetime.now()
         new_state = event.data.get("new_state")
         if not new_state or new_state.state in ("unknown", "unavailable"):
             return
-
         try:
             power = float(new_state.state)
         except ValueError:
